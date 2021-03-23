4d16ee710df43020b7d3edd781abe7db87b871db
Simon Wilmots <simon.wilmots1@gmail.com>
2021-03-23 21:11:08 +0100

    Added panda & CSV + Update README
    Added:
    - Basic panda implementation
    - CSV-file of the solar data
    Updated README

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ AI &amp; Machine learning
 - **Matplotlib** (Library for creating static, animated, and interactive visualizations in Python)
 ## Requirements
 *Here you will find all the information you need to get started with our AI / Machine learning*
+Install python (https://www.python.org/downloads/)
+Check if python is installed:
+`python --version`
+Install panda data library
+`pip install pandas`
+Verify installation of python pandas:
+`pip show pandas`
+Install matplotlib library
+`python -m pip install matplotlib`
--- data-visualisation.py
+++ data-visualisation.py
@@ -1,1 +1,25 @@
 #Data visualisation python code
+import pandas as pd
+import sys
+import matplotlib
+import matplotlib.pyplot as pyplot
+
+#Check versions of libraries for debugging purposes
+print("Python:\t", sys.version.split(' ')[0])
+print("Version:", sys.version_info, "\n")
+
+solar_data = pd.read_csv("./SolarData.csv")
+
+print(solar_data.info(), "\n" ,solar_data.describe(), "\n")
+
+#Get how many field values their are in the csv
+for field in solar_data:
+    print("******** Field:", field, "********")
+    print(solar_data[field].value_counts())
+    print()
+
+#Split date & time into seperate fields
+solar_data['date'] = [datetime.split(' ')[0][2:].strip() for datetime in solar_data['timestamp']]
+solar_data['time'] = [datetime.split(' ')[1].strip() for datetime in solar_data['timestamp']]
+print(solar_data)
+
