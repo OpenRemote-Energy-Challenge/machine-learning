3dca19a65aa90a1b1b1298fb9ad5389760040268
Simon Wilmots <simon.wilmots1@gmail.com>
2021-03-26 09:33:44 +0100

    Made machine-learning progress

--- machine-learning.py
+++ machine-learning.py
@@ -4,34 +4,33 @@ import matplotlib
 import matplotlib.pyplot as pyplot
 from sklearn.tree import DecisionTreeClassifier
 
-pd.set_option('display.max_rows', 10000)
-pd.set_option('display.max_columns', 10000)
-pd.set_option('display.width', 10000)
+"""def clean_data(dataset):
+    dataset = dataset.drop(columns=['timestamp'])
+    dataset.head()"""
 
-solar_data = pd.read_csv("./SolarData.csv")
+if __name__ == "__main__":
+   
+    pd.set_option('display.max_rows', 10000)
+    pd.set_option('display.max_columns', 10000)
+    pd.set_option('display.width', 10000)
 
-#print(solar_data)
+    solar_data = pd.read_csv("./SolarData.csv")
 
-solar_data['date'] = [datetime.split(' ')[0][2:].strip() for datetime in solar_data['timestamp']]
-solar_data['time'] = [datetime.split(' ')[1].strip() for datetime in solar_data['timestamp']]
+    solar_data['date'] = [datetime.split(' ')[0][2:].strip() for datetime in solar_data['timestamp']]
+    solar_data['time'] = [datetime.split(' ')[1].strip() for datetime in solar_data['timestamp']]
 
-totalPower = solar_data.loc[solar_data['attribute_name'] == 'totalPower']
-totalEnergy = solar_data.loc[solar_data['attribute_name'] == 'totalEnergy']
+    totalPower = solar_data.loc[solar_data['attribute_name'] == 'totalPower'].sort_values(by=['date', 'time'], ascending=True)
+    totalEnergy = solar_data.loc[solar_data['attribute_name'] == 'totalEnergy'].sort_values(by=['date', 'time'], ascending=True)
 
-#print(totalPower)
-count = 0
+    #totalPower = totalPower.drop(columns=['timestamp'])
+    print(totalPower)
 
-for power in totalEnergy['value']:
-    print(count, power)
-    if (count == 2939 or count == 5474):
-        print(power)
-    count = count + 1
+    #Code conventions:    X (capital X) = input of algorithm   y (small y) = output of algorithm
+    X = totalPower.drop(columns=['value'])
+    y = totalPower['value']
 
-#solar_data['totalPower'] = [power for power in totalPower['value']]
-#solar_data['totalEnergy'] = [energy for energy in totalEnergy['value']]    Length of values (2939) does not match length of index (5474)
+    model = DecisionTreeClassifier()
+    #model.fit(X, y)
+    
 
 
-#print(solar_data)
-
-def clean_data(dataset):
-    print(dataset)
