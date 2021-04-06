56b0a925fddd8254da1957df628c0727a1e0bd94
Simon Wilmots <simon.wilmots1@gmail.com>
2021-04-06 11:38:24 +0200

    Started TensorFlow neural network

--- api-integration.py
+++ api-integration.py
@@ -6,7 +6,7 @@ API_URL = "https://api.solcast.com.au/weather_sites/b185-4967-4cd7-c7b7/forecast
 try:
     resp = requests.get(API_URL)
     if resp.status_code != 200:     # This means something went wrong
-        raise requests.ConnectionError('GET /https://api.solcast.com.au/weather_sites/b185-4967-4cd7-c7b7/estimated_actuals?format=json/ {}'.format(resp.status_code))
+        raise requests.ConnectionError('GET https://api.solcast.com.au/weather_sites/b185-4967-4cd7-c7b7/forecasts?format=json {}'.format(resp.status_code))
     print(resp.json())
     
 except Exception as e:
